70a97af09f35c1ef58a903fce7b5bdea54db0b09
Brian "Beej Jorgensen" Hall <beej@beej.us>
2022-11-15 15:48:30 -0800

    manbreak option

--- bin/preproc_config.py
+++ bin/preproc_config.py
@@ -2,3 +2,5 @@ EXAMPLE_URL = "https://beej.us/guide/bgnet/examples/"
 EXERCISE_URL = "https://beej.us/guide/bgnet/exercises/"
 WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/"
 MAN_URL = "https://man.archlinux.org/man/"
+
+MANBREAK = "page"   # "page" or "rule"
